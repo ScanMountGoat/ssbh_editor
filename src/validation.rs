599f3cf74a8c2656137ba8ca2777956680a02055
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-07-05 18:28:00 -0500

    add test for required attributes

--- src/validation.rs
+++ src/validation.rs
@@ -17,26 +17,26 @@ pub struct ModelValidationErrors {
 
 impl ModelValidationErrors {
     pub fn from_model(model: &ModelFolder, shader_database: &ShaderDatabase) -> Self {
-        let matl_errors = model
-            .find_matl()
-            .map(|matl| validate_matl(matl, model.find_modl(), model.find_mesh(), shader_database))
-            .unwrap_or_default();
+        let mut validation = Self::default();
 
-        Self {
-            mesh_errors: Vec::new(),
-            skel_errors: Vec::new(),
-            matl_errors,
-            modl_errors: Vec::new(),
-            adj_errors: Vec::new(),
-            anim_errors: Vec::new(),
-            hlpb_errors: Vec::new(),
-            nutexb_errors: Vec::new(),
+        // Each validation check may add errors to multiple related files.
+        if let Some(matl) = model.find_matl() {
+            validate_required_attributes(
+                &mut validation,
+                matl,
+                model.find_modl(),
+                model.find_mesh(),
+                shader_database,
+            );
         }
+        validation
     }
 }
 
 pub struct MeshValidationError;
 pub struct SkelValidationError;
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum MatlValidationError {
     MissingRequiredVertexAttributes {
         entry_index: usize,
@@ -58,60 +58,125 @@ pub struct AnimValidationError;
 pub struct HlpbValidationError;
 pub struct NutexbValidationError;
 
-// TODO: How to incorporate this with the UI?
-fn validate_matl(
+fn validate_required_attributes(
+    validation: &mut ModelValidationErrors,
     matl: &MatlData,
     modl: Option<&ModlData>,
     mesh: Option<&MeshData>,
     shader_database: &ShaderDatabase,
-) -> Vec<MatlValidationError> {
+) {
+    // Both the modl and mesh should be present to determine material assignments.
     match (modl, mesh) {
-        (Some(modl), Some(mesh)) => matl
-            .entries
-            .iter()
-            .enumerate()
-            .filter_map(|(entry_index, entry)| {
+        (Some(modl), Some(mesh)) => {
+            for (entry_index, entry) in matl.entries.iter().enumerate() {
                 // TODO: make this a method of the database?
-                let program = shader_database.get(entry.shader_label.get(..24).unwrap_or(""))?;
+                if let Some(program) =
+                    shader_database.get(entry.shader_label.get(..24).unwrap_or(""))
+                {
+                    for o in mesh.objects.iter().filter(|o| {
+                        modl.entries
+                            .iter()
+                            .filter(|e| e.material_label == entry.material_label)
+                            .any(|e| {
+                                e.mesh_object_name == o.name
+                                    && e.mesh_object_sub_index == o.sub_index
+                            })
+                    }) {
+                        // Find attributes required by the shader not present in the mesh.
+                        let attribute_names: Vec<_> = o
+                            .texture_coordinates
+                            .iter()
+                            .map(|a| a.name.to_string())
+                            .chain(o.color_sets.iter().map(|a| a.name.to_string()))
+                            .collect();
 
-                Some(
-                    mesh.objects
-                        .iter()
-                        .filter(|o| {
-                            modl.entries
-                                .iter()
-                                .filter(|e| e.material_label == entry.material_label)
-                                .any(|e| {
-                                    e.mesh_object_name == o.name
-                                        && e.mesh_object_sub_index == o.sub_index
-                                })
-                        })
-                        .filter_map(move |o| {
-                            // TODO: check for missing attributes
-                            let attribute_names: Vec<_> = o
-                                .texture_coordinates
-                                .iter()
-                                .map(|a| a.name.to_string())
-                                .chain(o.color_sets.iter().map(|a| a.name.to_string()))
-                                .collect();
+                        let missing_attributes =
+                            program.missing_required_attributes(&attribute_names);
+                        if !missing_attributes.is_empty() {
+                            let error = MatlValidationError::MissingRequiredVertexAttributes {
+                                entry_index,
+                                mesh_name: o.name.clone(),
+                                missing_attributes,
+                            };
 
-                            let missing_attributes =
-                                program.missing_required_attributes(&attribute_names);
-                            if !missing_attributes.is_empty() {
-                                // TODO: Make this a function.
-                                Some(MatlValidationError::MissingRequiredVertexAttributes {
-                                    entry_index,
-                                    mesh_name: o.name.clone(),
-                                    missing_attributes,
-                                })
-                            } else {
-                                None
-                            }
-                        }),
-                )
-            })
-            .flatten()
-            .collect(),
-        _ => Vec::new(),
+                            validation.matl_errors.push(error);
+                        }
+                    }
+                }
+            }
+        }
+        _ => (),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ssbh_data::{
+        matl_data::MatlEntryData, mesh_data::MeshObjectData, modl_data::ModlEntryData,
+    };
+    use ssbh_wgpu::create_database;
+
+    use super::*;
+
+    #[test]
+    fn required_attributes_all_missing() {
+        let shader_database = create_database();
+        let matl = MatlData {
+            major_version: 1,
+            minor_version: 6,
+            entries: vec![MatlEntryData {
+                material_label: "a".to_string(),
+                shader_label: "SFX_PBS_010002000800824f_opaque".to_string(),
+                blend_states: Vec::new(),
+                floats: Vec::new(),
+                booleans: Vec::new(),
+                vectors: Vec::new(),
+                rasterizer_states: Vec::new(),
+                samplers: Vec::new(),
+                textures: Vec::new(),
+            }],
+        };
+        let mesh = MeshData {
+            major_version: 1,
+            minor_version: 10,
+            objects: vec![MeshObjectData {
+                name: "object1".to_string(),
+                sub_index: 0,
+                ..Default::default()
+            }],
+        };
+        let modl = ModlData {
+            major_version: 1,
+            minor_version: 0,
+            model_name: String::new(),
+            skeleton_file_name: String::new(),
+            material_file_names: Vec::new(),
+            animation_file_name: None,
+            mesh_file_name: String::new(),
+            entries: vec![ModlEntryData {
+                mesh_object_name: "object1".to_owned(),
+                mesh_object_sub_index: 0,
+                material_label: "a".to_string(),
+            }],
+        };
+
+        let mut validation = ModelValidationErrors::default();
+        validate_required_attributes(
+            &mut validation,
+            &matl,
+            Some(&modl),
+            Some(&mesh),
+            &shader_database,
+        );
+
+        // TODO: Add a mesh error as well?
+        assert_eq!(
+            vec![MatlValidationError::MissingRequiredVertexAttributes {
+                entry_index: 0,
+                mesh_name: "object1".to_string(),
+                missing_attributes: vec!["map1".to_string(), "uvSet".to_string()]
+            }],
+            validation.matl_errors
+        );
     }
 }
