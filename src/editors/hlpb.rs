6d998fea9e96c35800ba8030fac803d00c5e5751
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-08-15 14:48:04 -0500

    use vertical layout for hlpb editor, add missing fields

--- src/editors/hlpb.rs
+++ src/editors/hlpb.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use egui::{CollapsingHeader, DragValue, ScrollArea};
+use egui::{CollapsingHeader, DragValue, Grid, ScrollArea, Ui};
 use log::error;
 use rfd::FileDialog;
 use ssbh_data::{prelude::*, Vector3, Vector4};
@@ -78,135 +78,193 @@ pub fn hlpb_editor(
     (open, changed)
 }
 
-fn orient_constraints(ui: &mut egui::Ui, hlpb: &mut HlpbData, skel: Option<&SkelData>) -> bool {
-    // TODO: Rework this layout to be more compact.
+fn orient_constraints(ui: &mut Ui, hlpb: &mut HlpbData, skel: Option<&SkelData>) -> bool {
     let mut changed = false;
     CollapsingHeader::new("Orient Constraints")
         .default_open(true)
         .show(ui, |ui| {
-            egui::Grid::new("orient").striped(true).show(ui, |ui| {
-                ui.heading("Name");
-                ui.heading("Parent 1");
-                ui.heading("Parent 2");
-                ui.heading("Source");
-                ui.heading("Target");
-                ui.heading("Unk Type");
-                ui.heading("Constraint Axes");
-                ui.heading("Quat 1");
-                ui.heading("Quat 2");
-                ui.heading("Range Min");
-                ui.heading("Range Max");
-                ui.end_row();
+            for (i, o) in hlpb.orient_constraints.iter_mut().enumerate() {
+                let id = egui::Id::new("orient").with(i);
 
-                for (i, orient) in hlpb.orient_constraints.iter_mut().enumerate() {
-                    let id = egui::Id::new("orient").with(i);
+                // Append the helper bone name to make it easier to find constraints.
+                CollapsingHeader::new(format!("{} ({})", o.name, o.target_bone_name))
+                    .id_source(id.with(&o.name))
+                    .default_open(false)
+                    .show(ui, |ui| {
+                        Grid::new(id).show(ui, |ui| {
+                            ui.label("Name");
+                            changed |= ui.text_edit_singleline(&mut o.name).changed();
+                            ui.end_row();
 
-                    ui.label(&orient.name);
-                    changed |=
-                        bone_combo_box(ui, &mut orient.parent_bone1_name, id.with(0), skel, &[]);
-                    changed |=
-                        bone_combo_box(ui, &mut orient.parent_bone2_name, id.with(1), skel, &[]);
-                    changed |=
-                        bone_combo_box(ui, &mut orient.source_bone_name, id.with(2), skel, &[]);
-                    changed |=
-                        bone_combo_box(ui, &mut orient.target_bone_name, id.with(3), skel, &[]);
+                            ui.label("Parent 1");
+                            changed |=
+                                bone_combo_box(ui, &mut o.parent_bone1_name, id.with(0), skel, &[]);
+                            ui.end_row();
 
-                    egui::ComboBox::from_id_source(id.with(4))
-                        .selected_text(orient.unk_type.to_string())
-                        .show_ui(ui, |ui| {
-                            changed |= ui.selectable_value(&mut orient.unk_type, 1, "1").changed();
-                            changed |= ui.selectable_value(&mut orient.unk_type, 2, "2").changed();
+                            ui.label("Parent 2");
+                            changed |=
+                                bone_combo_box(ui, &mut o.parent_bone2_name, id.with(1), skel, &[]);
+                            ui.end_row();
+
+                            ui.label("Source");
+                            changed |=
+                                bone_combo_box(ui, &mut o.source_bone_name, id.with(2), skel, &[]);
+                            ui.end_row();
+
+                            ui.label("Target");
+                            changed |=
+                                bone_combo_box(ui, &mut o.target_bone_name, id.with(3), skel, &[]);
+                            ui.end_row();
+
+                            ui.label("Unk Type");
+                            egui::ComboBox::from_id_source(id.with(4))
+                                .selected_text(o.unk_type.to_string())
+                                .show_ui(ui, |ui| {
+                                    changed |=
+                                        ui.selectable_value(&mut o.unk_type, 1, "1").changed();
+                                    changed |=
+                                        ui.selectable_value(&mut o.unk_type, 2, "2").changed();
+                                });
+                            ui.end_row();
+
+                            ui.label("Constraint Axes");
+                            changed |= edit_vector3(ui, id.with(5), &mut o.constraint_axes);
+                            ui.end_row();
+
+                            ui.label("Quat 1");
+                            changed |= edit_vector4(ui, id.with(6), &mut o.quat1);
+                            ui.end_row();
+
+                            ui.label("Quat 2");
+                            changed |= edit_vector4(ui, id.with(7), &mut o.quat2);
+                            ui.end_row();
+
+                            ui.label("Range Min");
+                            changed |= edit_vector3(ui, id.with(8), &mut o.range_min);
+                            ui.end_row();
+
+                            ui.label("Range Max");
+                            changed |= edit_vector3(ui, id.with(9), &mut o.range_max);
+                            ui.end_row();
                         });
-
-                    changed |= edit_vector3(ui, id.with(5), &mut orient.constraint_axes);
-                    changed |= edit_vector4(ui, id.with(6), &mut orient.quat1);
-                    changed |= edit_vector4(ui, id.with(7), &mut orient.quat2);
-                    changed |= edit_vector3(ui, id.with(8), &mut orient.range_min);
-                    changed |= edit_vector3(ui, id.with(9), &mut orient.range_max);
-
-                    ui.end_row();
-                }
-            });
+                    });
+            }
         });
     changed
 }
 
-fn edit_vector3(ui: &mut egui::Ui, id: egui::Id, value: &mut Vector3) -> bool {
-    let mut changed = false;
-    ui.horizontal(|ui| {
-        changed |= ui
-            .add(DragSlider::new(id.with("x"), &mut value.x).width(40.0))
-            .changed();
-        changed |= ui
-            .add(DragSlider::new(id.with("y"), &mut value.y).width(40.0))
-            .changed();
-        changed |= ui
-            .add(DragSlider::new(id.with("z"), &mut value.z).width(40.0))
-            .changed();
-    });
-    changed
-}
-
-fn edit_vector4(ui: &mut egui::Ui, id: egui::Id, value: &mut Vector4) -> bool {
-    let mut changed = false;
-    ui.horizontal(|ui| {
-        changed |= ui
-            .add(DragSlider::new(id.with("x"), &mut value.x).width(40.0))
-            .changed();
-        changed |= ui
-            .add(DragSlider::new(id.with("y"), &mut value.y).width(40.0))
-            .changed();
-        changed |= ui
-            .add(DragSlider::new(id.with("z"), &mut value.z).width(40.0))
-            .changed();
-        changed |= ui
-            .add(DragSlider::new(id.with("w"), &mut value.z).width(40.0))
-            .changed();
-    });
-    changed
-}
-
-fn aim_constraints(ui: &mut egui::Ui, hlpb: &mut HlpbData, skel: Option<&SkelData>) -> bool {
-    // TODO: Add missing fields.
+fn aim_constraints(ui: &mut Ui, hlpb: &mut HlpbData, skel: Option<&SkelData>) -> bool {
     let mut changed = false;
     CollapsingHeader::new("Aim Constraints")
         .default_open(true)
         .show(ui, |ui| {
-            egui::Grid::new("aim").striped(true).show(ui, |ui| {
-                ui.heading("Name");
-                ui.heading("Aim 1");
-                ui.heading("Aim 2");
-                ui.heading("Type 1");
-                ui.heading("Type 2");
-                ui.heading("Target 1");
-                ui.heading("Target 2");
-                ui.heading("Unk1");
-                ui.heading("Unk2");
-                ui.heading("Quat 1");
-                ui.heading("Quat 2");
-                ui.end_row();
+            for (i, aim) in hlpb.aim_constraints.iter_mut().enumerate() {
+                let id = egui::Id::new("aim").with(i);
 
-                for (i, aim) in hlpb.aim_constraints.iter_mut().enumerate() {
-                    let id = egui::Id::new("aim").with(i);
+                // Append the helper bone names to make it easier to find constraints.
+                CollapsingHeader::new(format!(
+                    "{} ({} / {})",
+                    aim.name, aim.target_bone_name1, aim.target_bone_name2
+                ))
+                .id_source(id.with(&aim.name))
+                .default_open(false)
+                .show(ui, |ui| {
+                    egui::Grid::new(id).show(ui, |ui| {
+                        ui.label("Name");
+                        changed |= ui.text_edit_singleline(&mut aim.name).changed();
+                        ui.end_row();
 
-                    ui.label(&aim.name);
-                    changed |= bone_combo_box(ui, &mut aim.aim_bone_name1, id.with(0), skel, &[]);
-                    changed |= bone_combo_box(ui, &mut aim.aim_bone_name2, id.with(1), skel, &[]);
-                    changed |=
-                        bone_combo_box(ui, &mut aim.aim_type1, id.with(2), skel, &["DEFAULT"]);
-                    changed |=
-                        bone_combo_box(ui, &mut aim.aim_type2, id.with(3), skel, &["DEFAULT"]);
-                    changed |=
-                        bone_combo_box(ui, &mut aim.target_bone_name1, id.with(4), skel, &[]);
-                    changed |=
-                        bone_combo_box(ui, &mut aim.target_bone_name2, id.with(5), skel, &[]);
-                    changed |= ui.add(DragValue::new(&mut aim.unk1)).changed();
-                    changed |= ui.add(DragValue::new(&mut aim.unk2)).changed();
-                    changed |= edit_vector4(ui, id.with(6), &mut aim.quat1);
-                    changed |= edit_vector4(ui, id.with(7), &mut aim.quat2);
-                    ui.end_row();
-                }
-            });
+                        ui.label("Aim 1");
+                        changed |=
+                            bone_combo_box(ui, &mut aim.aim_bone_name1, id.with(0), skel, &[]);
+                        ui.end_row();
+
+                        ui.label("Aim 2");
+                        changed |=
+                            bone_combo_box(ui, &mut aim.aim_bone_name2, id.with(1), skel, &[]);
+                        ui.end_row();
+
+                        ui.label("Aim Type 1");
+                        changed |=
+                            bone_combo_box(ui, &mut aim.aim_type1, id.with(2), skel, &["DEFAULT"]);
+                        ui.end_row();
+
+                        ui.label("Aim Type 2");
+                        changed |=
+                            bone_combo_box(ui, &mut aim.aim_type2, id.with(3), skel, &["DEFAULT"]);
+                        ui.end_row();
+
+                        ui.label("Target 1");
+                        changed |=
+                            bone_combo_box(ui, &mut aim.target_bone_name1, id.with(4), skel, &[]);
+                        ui.end_row();
+
+                        ui.label("Target 2");
+                        changed |=
+                            bone_combo_box(ui, &mut aim.target_bone_name2, id.with(5), skel, &[]);
+                        ui.end_row();
+
+                        ui.label("Unk1");
+                        changed |= ui.add(DragValue::new(&mut aim.unk1)).changed();
+                        ui.end_row();
+
+                        ui.label("Unk2");
+                        changed |= ui.add(DragValue::new(&mut aim.unk2)).changed();
+                        ui.end_row();
+
+                        ui.label("Aim");
+                        changed |= edit_vector3(ui, id.with(6), &mut aim.aim);
+                        ui.end_row();
+
+                        ui.label("Up");
+                        changed |= edit_vector3(ui, id.with(7), &mut aim.up);
+                        ui.end_row();
+
+                        ui.label("Quat 1");
+                        changed |= edit_vector4(ui, id.with(8), &mut aim.quat1);
+                        ui.end_row();
+
+                        ui.label("Quat 2");
+                        changed |= edit_vector4(ui, id.with(9), &mut aim.quat2);
+                        ui.end_row();
+                    });
+                });
+            }
         });
+    changed
+}
+
+fn edit_vector3(ui: &mut Ui, id: egui::Id, value: &mut Vector3) -> bool {
+    let mut changed = false;
+    ui.horizontal(|ui| {
+        changed |= ui
+            .add(DragSlider::new(id.with("x"), &mut value.x).width(40.0))
+            .changed();
+        changed |= ui
+            .add(DragSlider::new(id.with("y"), &mut value.y).width(40.0))
+            .changed();
+        changed |= ui
+            .add(DragSlider::new(id.with("z"), &mut value.z).width(40.0))
+            .changed();
+    });
+    changed
+}
+
+fn edit_vector4(ui: &mut Ui, id: egui::Id, value: &mut Vector4) -> bool {
+    let mut changed = false;
+    ui.horizontal(|ui| {
+        changed |= ui
+            .add(DragSlider::new(id.with("x"), &mut value.x).width(40.0))
+            .changed();
+        changed |= ui
+            .add(DragSlider::new(id.with("y"), &mut value.y).width(40.0))
+            .changed();
+        changed |= ui
+            .add(DragSlider::new(id.with("z"), &mut value.z).width(40.0))
+            .changed();
+        changed |= ui
+            .add(DragSlider::new(id.with("w"), &mut value.w).width(40.0))
+            .changed();
+    });
     changed
 }
