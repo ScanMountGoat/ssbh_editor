3c4670b8ff629c9203a6b25d8c4a5fe4a34435aa
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2024-07-04 10:25:32 -0500

    update dependencies

--- src/app/window/log.rs
+++ src/app/window/log.rs
@@ -14,10 +14,8 @@ pub fn log_window(ctx: &Context, open: &mut bool) {
                         ui.horizontal(|ui| {
                             log_level_icon(ui, level);
                             // binrw formats backtraces, which isn't supported by egui font rendering.
-                            // TODO: Avoid clone?
-                            let clean_message = strip_ansi_escapes::strip(message)
-                                .map(|m| String::from_utf8_lossy(&m).to_string())
-                                .unwrap_or_else(|_| message.clone());
+                            let clean_message = strip_ansi_escapes::strip(message);
+                            let clean_message = String::from_utf8_lossy(&clean_message);
                             ui.add(Label::new(clean_message).wrap());
                         });
                     }
